70e181e1bea62f8cfe86e25b8f86f6320c5179a9
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 21:44:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-//your JS code here. If required.
-    var style = document.createElement("style");
-    style.textContent = "#line{animation: rotate 4s;"+
-        "animation-fill-mode: forwards;"+
-        "animation-timing-function:linear;}"+
-        "@keyframes rotate{"+
-            "from{transform: rotate(0deg);}"+
-            "to{transform: rotate(500deg);}"+
-        "}";
-    document.body.appendChild(style);
+const line = document.getElementById('line');
+
+let angle = 0;
+
+function rotateLine() {
+
+  angle++;
+
+  line.style.transform = `rotate(${angle}deg)`;
+
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
